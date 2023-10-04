fe5e5c840e711b693ba0239c040fb4fdfbeffedd
atomjin <kazukiryouto@gmail.com>
2023-10-05 01:33:30 +0700

    decorate-signup

---
+++ android/app/src/main/kotlin/com/FITGAP/fitgap/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.FITGAP.fitgap
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -11,95 +11,107 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0x00060239),
-      body: Center(
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 100,
-            ),
-      
-            //logo
-            ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: Image.asset(
-                'assets/icons/AppIcon.png',
-                height: 150,
-                width: 150,
+    //image background
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage('assets/icons/bg.png'), fit: BoxFit.cover),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Center(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 20,
               ),
-            ),
-      
-            const SizedBox(
-              height: 50,
-            ),
-      
-            //login text
-            const Text(
-              'Login',
-              style: TextStyle(fontSize: 40, color: Colors.white),
-            ),
-      
-            const SizedBox(
-              height: 50,
-            ),
-      
-            //email textfield
-            MyTextField(
-                controller: emailController,
-                hintText: 'Email Address',
-                obscureText: false),
-      
-            const SizedBox(
-              height: 20,
-            ),
-      
-            //password textfield
-            MyTextField(
-                controller: passwordController,
-                hintText: 'Password',
-                obscureText: true),
-      
-            const SizedBox(
-              height: 20,
-            ),
-      
-            //remember password checkbox
-            //forget password
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 25),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    '',
-                    style: TextStyle(color: Colors.white),
+
+              //logo
+              ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.asset(
+                  'assets/icons/plandarlogo.png',
+                  height: 110,
+                  width: 500,
+                ),
+              ),
+
+              const SizedBox(
+                  //height: 50,
                   ),
-                  Text('Forgot passsword', style: TextStyle(color: Colors.white))
-                ],
+
+              //login text
+              const Text(
+                'Login',
+                style: TextStyle(fontSize: 40, color: Colors.white),
               ),
-            ),
-      
-            const SizedBox(
-              height: 25,
-            ),
-      
-            //login button
-            ElevatedButton.icon(
-              icon: const Icon(Icons.lock_open),
-              label: const Text('Login'),
-              onPressed: () {},
-            ),
-      
-            const SizedBox(
-              height: 25,
-            ),
-      
-            //or login with
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 25),
-              child: Row(
-                children: [
+
+              const SizedBox(
+                height: 50,
+              ),
+
+              //email textfield
+              MyTextField(
+                  controller: emailController,
+                  hintText: 'Email Address',
+                  obscureText: false),
+
+              const SizedBox(
+                height: 20,
+              ),
+
+              //password textfield
+              MyTextField(
+                  controller: passwordController,
+                  hintText: 'Password',
+                  obscureText: true),
+
+              const SizedBox(
+                height: 20,
+              ),
+
+              //remember password checkbox
+              //forget password
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 25),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      '',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    Text('Forgot passsword',
+                        style: TextStyle(color: Colors.white))
+                  ],
+                ),
+              ),
+
+              const SizedBox(
+                height: 25,
+              ),
+
+              //login button
+              ElevatedButton.icon(
+                icon: const Icon(Icons.lock_open),
+                label: const Text(
+                  'Login',
+                  style: TextStyle(fontSize: 20),
+                ),
+                onPressed: () {}, //login button change size
+                style: ElevatedButton.styleFrom(
+                  fixedSize: const Size(300, 50),
+                ),
+              ),
+
+              const SizedBox(
+                height: 25,
+              ),
+
+              //or login with
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 25),
+                child: Row(children: [
                   //Left divider
                   Expanded(
                     child: Divider(
@@ -107,7 +119,7 @@ class LoginPage extends StatelessWidget {
                       color: Colors.white,
                     ),
                   ),
-      
+
                   //Text
                   Padding(
                     padding: EdgeInsets.symmetric(horizontal: 10),
@@ -116,7 +128,7 @@ class LoginPage extends StatelessWidget {
                       style: TextStyle(color: Colors.white),
                     ),
                   ),
-      
+
                   //Right divider
                   Expanded(
                     child: Divider(
@@ -124,24 +136,24 @@ class LoginPage extends StatelessWidget {
                       color: Colors.white,
                     ),
                   ),
-                ]
+                ]),
               ),
-            ),
-      
-            const SizedBox(
-              height: 25,
-            ),
 
-            //google + facebook login buttons
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SquareTile(imagePath: 'assets/icons/GoogleIcon.png'),
-                SquareTile(imagePath: 'assets/icons/FacebookIcon.png'),
-              ],
-            )
-            //don't have account? create now
-          ],
+              const SizedBox(
+                height: 25,
+              ),
+
+              //google + facebook login buttons
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SquareTile(imagePath: 'assets/icons/GoogleIcon.png'),
+                  SquareTile(imagePath: 'assets/icons/FacebookIcon.png'),
+                ],
+              )
+              //don't have account? create now
+            ],
+          ),
         ),
       ),
     );
