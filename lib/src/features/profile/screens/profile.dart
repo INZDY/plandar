dc62e9278039e1a78a01a510bfeef0fdf4c152f6
Aphivit Jirarattanametharkorn <aphivit12507.jira@gmail.com>
2023-10-10 17:42:56 +0700

    Simple profile display(only username field). Firestore data query function.

--- lib/src/features/profile/screens/profile.dart
+++ lib/src/features/profile/screens/profile.dart
@@ -1,14 +1,62 @@
+// import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fitgap/src/utils/firestore/firestore.dart';
 import 'package:flutter/material.dart';
 
-class Profile extends StatelessWidget {
+class Profile extends StatefulWidget {
   const Profile({super.key});
+
+  @override
+  State<Profile> createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  final _firestoreService = FirestoreService();
+
+  Map<String, dynamic>? _userData;
+
+  Future loadUserData() async {
+    final userData = await _firestoreService.getUserData();
+
+    setState(() {
+      _userData = userData ?? {};
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadUserData();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Profile'),
+        title: const Text(
+          'Profile',
+          style: TextStyle(color: Colors.black),
+        ),
+        // backgroundColor: Colors.transparent,
+        // shadowColor: Colors.transparent,
+        centerTitle: true,
       ),
+      body: SafeArea(
+          child: ListView(
+        children: [
+          ListTile(
+            title: const Text('Username'),
+            subtitle: Text('${_userData?['username']}'),
+          ),
+        ],
+
+        //map all to listtiles
+        // children: (_userData?.entries ?? []).map((entry) {
+        //   return ListTile(
+        //     title: Text(entry.key),
+        //     subtitle: Text(entry.value.toString()),
+        //   );
+        // }).toList(),
+      )),
     );
   }
 }
--- lib/src/utils/firestore/firestore.dart
+++ lib/src/utils/firestore/firestore.dart
@@ -1,11 +1,39 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class FirestoreService {
-  //get collection of events
+  //firebase firestore instance
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
 
-  //CREATE: add user details
-  final CollectionReference userDetails =
-      FirebaseFirestore.instance.collection('');
+  User? _user;
+
+  //read all
+  Future getUserData() async {
+    //check if authenticated
+    if (_auth.currentUser != null) {
+      //get current user
+      _user = _auth.currentUser;
+
+      //Query Firestore for user data => first as query snapshot
+      final querySnapshot = await _db
+          .collection('users')
+          .where('email', isEqualTo: _user!.email)
+          .get();
+
+      if (querySnapshot.docs.isNotEmpty) {
+        final userData = querySnapshot.docs.first.data();
+        //Get and return user data
+        return userData;
+      } else {
+        return {};
+      }
+    }
+  }
+
+  // //CREATE: add user details
+  // final CollectionReference userDetails =
+  //     FirebaseFirestore.instance.collection('');
 
   //CREATE: add event
 
